94e14f4c9b26429aadd5a2fe3300da2c53d85f5e
Jakevin <kevin5251984@gmail.com>
2024-10-30 18:25:23 +0900

    fix(line-checkin): base path

--- line-checkin/assets/AboutView-DmhLkqMt.js
+++
@@ -1,1 +0,0 @@
-import{c as a,a as c,o as r}from"./index-4odl6YJW.js";const _=(e,t)=>{const o=e.__vccOpts||e;for(const[s,n]of t)o[s]=n;return o},i={},l={class:"about"};function u(e,t){return r(),a("div",l,t[0]||(t[0]=[c("h1",null,"This is an about page",-1)]))}const d=_(i,[["render",u]]);export{d as default};
